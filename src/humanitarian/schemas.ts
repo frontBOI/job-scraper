07148d325df3c52e3a237fcea820f4c32021be14
Tom Blanchet <contact.tomblanchet@gmail.com>
2024-11-24 14:19:18 +0100

    🩹 fix: typing

--- src/humanitarian/schemas.ts
+++ src/humanitarian/schemas.ts
@@ -1,7 +1,15 @@
 import ScraperUserDefinedOptionsSchema from '../scraper/schemas'
+import { HumanitarianScrapingSource } from './types'
 
-import { object, string } from 'zod'
+import { array, nativeEnum, object, string } from 'zod'
+
+const CompleteJobSchema = object({
+  id: string(),
+  name: string(),
+  link: string(),
+  source: nativeEnum(HumanitarianScrapingSource),
+})
 
 export default object({
-  opportunitiesIdsToSkip: string().optional(),
+  opportunitiesIdsToSkip: array(CompleteJobSchema).optional(),
 }).merge(ScraperUserDefinedOptionsSchema) // extend default scraper options
